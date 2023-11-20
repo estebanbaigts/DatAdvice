042a3d750a81e1ebb651d75519186d0f11d04894
Esteban <esteban.baigts@epitech.eu>
2023-11-21 00:24:09 +0100

    oui

---
+++ nom-du-projet/src/acceuil/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './home.css';
+
+function Home() {
+  return (
+    <div className="App">
+      <h1>Bonjour à tous</h1>
+    </div>
+  );
+}
+
+export default Home;
---
+++ nom-du-projet/src/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import NavBar from './acceuil/navbar/navbar';
+import Home from './acceuil/home';
+import './app.css';
+
+const App = () => {
+    return (
+        <div>
+            <NavBar />
+            <Home />
+        </div>
+    );
+};
+
+export default App;
